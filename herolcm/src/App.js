9eb8cd4a6677978b7bd19a5c33b748b6647e8680
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-12-14 19:20:11 -0600

    Trabajo en equipo de LCOM de Hero

---
+++ herolcm/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <section id="juego">
+    <div className="container">
+    <div className="info">
+      <h1>El juego de Mario Kart</h1>
+      <h2>Es un clásico de los videojuegos donde tendras muchas horas de diversión</h2>
+      <p>Una  carrera donde tendras muchos obstaculo, metas y objetivos, en donde sea, en consola
+        o en línea pasaran estos obstaculos de entretenimiento
+      </p>
+      <a href="https://www.youtube.com/watch?v=P7FFEn6srB4">Click al videoclip</a>
+    </div>
+    </div>
+    </section>
+  );
+}
+
+export default App;
